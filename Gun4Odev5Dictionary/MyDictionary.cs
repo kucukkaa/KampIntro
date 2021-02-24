6ce5b6a9081dda366a28858afe0668ae62f8ec04
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-02-24 17:36:31 +0300

    gun4odev5

---
+++ Gun4Odev5Dictionary/MyDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun4Odev5Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+    }
+}
---
+++ Gun4Odev5Dictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gun4Odev5Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
