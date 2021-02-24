e8b3a8212407da3007fdc8f2ffc6f2c9eff2ec55
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-02-24 16:40:11 +0300

    gun4odev2 eklendikten sonra

---
+++ Gun4Odev2Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Gun4Odev2Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FristName = "ali", LastName = "veli", City = "Ankara" };//defult constructor çalışır.
+            Customer customer2 = new Customer(2, "ahmet", "mehmet", "İstanbul");//parametre bekleyen constructor çalışır. ancak bu constructora gelen parametreler için constructor içinde
+            //ilgili veriye atama yapılması gereklidir. ancak bu şekilde customer'nin özelliklerine ulaşılabilir. 
+
+            
+
+        }
+
+        
+    }
+
+    class Customer
+    {
+
+        //defult constructor
+        public Customer()
+        {
+
+        }
+        
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            FristName = firstName;
+            Id = id;
+            LastName = lastName;
+            City = city;
+            
+            Console.WriteLine("yapıcı blok çalıştırıldı.");
+        }
+
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
