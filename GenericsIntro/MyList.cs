2b851c3dd7892d0897367f2ab3cf8e30f91026e7
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-02-24 13:57:06 +0300

    24.02.2021v2

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -21,7 +21,17 @@ namespace GenericsIntro
                 items[i] = tempArray[i];
             }
 
-            items[items.Length-1]= item
+            items[items.Length - 1] = item;
+
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] Items
+        {
+            get { return items; }
         }
     }
 }
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -6,7 +6,19 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
-            
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
