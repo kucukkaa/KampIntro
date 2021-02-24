ad52ab18b2fd0d2b41b62a2fecf1b36667cedfb3
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-02-24 13:22:21 +0300

    24.02.2021

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] {"engin", "murat", "kerem", "halil"};
+            Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string>() {"engin", "murat", "kerem", "halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            isimler2.Add("ilker");
+
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[4]);
+
+
+        }
+    }
+}
