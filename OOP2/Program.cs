6c208205a6d08722939840ddace3ea9069e53564
ALİERK KÜÇÜK <alierkkucuk@hotmail.com>
2021-02-25 00:04:21 +0300

    25.02.2021

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12356612";
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "Veli";
+            musteri1.TcNo = "12312343645756";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "3434123";
+            musteri2.VergiNo = "34243412";
+            musteri2.SirketAdi = "Kodlama.io";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            // ihtiyacKrediManager.Hesapla();
+
+            // TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            // KonutKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();             //bir interface kendisini implemente eden bir sınıfın referansını tutabilir.
+            //konutKrediManager.Hesapla();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {konutKrediManager};
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
