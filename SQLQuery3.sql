fd3ad20b3c890f578b5f2688e3cc96b2b6a6f8ef
Alierk KÜÇÜK <Alierk KK@DESKTOP-OOU3I1N>
2021-03-07 17:49:51 +0300

    07.03.2021 sql

---
+++ SQLQuery3.sql
@@ -0,0 +1,3 @@
+﻿select p.ProductName, sum(od.UnitPrice*od.Quantity) from Products p inner join [Order Details] od on  p.ProductID = od.ProductID group by p.ProductName
+
+select * from [Order Details] where ProductID = 14
