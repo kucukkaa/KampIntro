44680705bcfab9796aea67b4cb84148f3450b451
ALİERK KÜÇÜK <alierkkucuk@hotmail.com>
2021-03-14 00:00:30 +0300

    13.03.2021

---
+++ Gun8Odev2EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun8Odev2EntityFrameworkDemo
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server= (localdb)\mssqllocaldb; database= Northwind; Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Gun8Odev2EntityFrameworkDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Gun8Odev2EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADO.NET 
+            //ENTITY FRAMEWORK - ORM - OBJECT RELATIONAL MAPPING ALTERNATİFLER NHIBERNATE, DAPPER
+
+
+            //GetAll();
+
+            GetProductsByCategory(1);
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            foreach (var item in northwindContext.Products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
