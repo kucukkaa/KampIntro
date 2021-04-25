af1a77b13b793fa8ae14f4ccc8e35fffa54029ed
kucukkaa <50020610+kucukkaa@users.noreply.github.com>
2021-04-25 15:46:06 +0300

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,3 +0,0 @@
-﻿select p.ProductName, sum(od.UnitPrice*od.Quantity) from Products p inner join [Order Details] od on  p.ProductID = od.ProductID group by p.ProductName
-
-select * from [Order Details] where ProductID = 14
